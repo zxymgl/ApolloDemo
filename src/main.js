02da826b06eead6cccfb0b93fbac21ed7d6e0725
zhouxian <zhouxian@cjtc.net.cn>
2016-10-19 13:43:28 +0800

    改成 github的cookies

--- src/main.js
+++ src/main.js
@@ -11,10 +11,12 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client'
 
 import { ApolloProvider } from 'react-apollo'
 
+const  sesssionValue = 'dUM0d0J1aTdLZXdlM09hZm1qU3FyMnhjK2FpalU5TDliYkNsOU0xTUJmUDBQZDFkUWtCci8vc0x1M1BuN1pzbVh0dm04M0F4QjBLL0YrTnU4ZUZ2TkJsVk1wemM4elVoZU05TkEyOWxOZUFsMVdKT1dBVisrZlRVSUdMR08vdW1VaVZWMlIxaW9qN1lEanlmbXJPenhKSVdCS25jQ2dFVVhVNFVydEZmei9jZHJTMmZ1RUNUMzIzcllSSmhqZGN4QkVHcEFWUjJxemRBNGNHSnpHK0RGNGM5bVVmYnlVTzdXeTJuY1lycjRUcjQ0emt4OWlGNFdNSFdaZFZZbXRLY20wS0V0VHBxK09GRjRqQ0tMZkJMNGh5MXpKTTRLWEdteHFqQk03cEZnUlh6Zy9QcmhFQUdxQTVMVHhqQmVWS3VDb3h0dmxKL3J1VWxQaXV2T2p4YWJoZDlFd0dnYVYxMTU1WmtLMlhZWEJZPS0tY3VrRGlpQkJnK0pDSDd2NzBhSUdLQT09--33ff21a5a75118afcbfbd097dde6155cd1926982'
 const networkInterface = createNetworkInterface({
-  uri: '/graphql',
+  uri: 'graphql-explorer.githubapp.com',
   opts: {
-    credentials: 'same-origin',
+    _graphql_explorer_session: sesssionValue,
+
   },
   transportBatching: true,
 });
